6ec8b8d425ac33de4b7032d324c6cc695d22e7d5
gvenet <venetguillaume@gmail.com>
2021-08-16 12:22:32 +0200

    1

--- inc/ft_vector.hpp
+++ inc/ft_vector.hpp
@@ -3,18 +3,74 @@
 
 #include <iostream>
 
-class Ft_vector
-{
-private:
-public:
-	Ft_vector();
-	Ft_vector(Ft_vector const &cpy);
-	Ft_vector &operator=(Ft_vector const &op);
-	virtual ~Ft_vector();
+template <typename T>class FtVector {
 
-	//========GETTERS========
-	//========SETTERS========
-	//========METHODS========
+private:
+	T *arr;
+	int capacity;
+	int current;
+
+public:
+	FtVector() {
+		arr = new T[1];
+		capacity = 1;
+		current = 0;
+	}
+
+	void push(T data) {
+		if (current == capacity){
+			T *temp = new T[2 * capacity];
+			for (int i = 0; i < capacity; i++){
+				temp[i] = arr[i];
+			}
+			delete[] arr;
+			capacity *= 2;
+			arr = temp;
+		}
+		arr[current] = data;
+		current++;
+	}
+
+	void push(int data, int index){
+		if (index == capacity)
+			push(data);
+		else
+			arr[index] = data;
+	}
+
+	T get(int index){
+		if (index < current)
+			return arr[index];
+	}
+
+	void pop() { 
+		current--; }
+
+	int size() { 
+		return current; }
+
+	int getcapacity() { 
+		return capacity; }
+	
+	void print(){
+		for (int i = 0; i < current; i++){
+			std::cout << arr[i] << " ";
+		}
+		std::cout << std::endl;
+	}
 };
 
+// class Ft_vector
+// {
+// private:
+// public:
+// 	Ft_vector();
+// 	Ft_vector(Ft_vector const &cpy);
+// 	Ft_vector &operator=(Ft_vector const &op);
+// 	virtual ~Ft_vector();
+
+// 	//========GETTERS========
+// 	//========SETTERS========
+// 	//========METHODS========
+// };
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,8 +2,57 @@
 #include "../inc/ft_vector.hpp"
 
 
-int main(void)
+int main()
 {
-	Ft_vector A;	
-	return 0;
+    FtVector<int> v;
+    FtVector<char> v1;
+    v.push(10);
+    v.push(20);
+    v.push(30);
+    v.push(40);
+    v.push(50);
+    v1.push(71);
+    v1.push(72);
+    v1.push(73);
+    v1.push(74);
+ 
+    std::cout << "Vector size : " << v.size() << std::endl;
+    std::cout << "Vector capacity : " << v.getcapacity() << std::endl;
+ 
+    std::cout << "Vector elements : ";
+    v.print();
+ 
+    v.push(100, 1);
+ 
+    std::cout << "\nAfter updating 1st index" << std::endl;
+ 
+    std::cout << "Vector elements of type int : " << std::endl;
+    v.print();
+    // This was possible because we used templates
+    std::cout << "Vector elements of type char : " << std::endl;
+    v1.print();
+    std::cout << "Element at 1st index of type int: " << v.get(1)
+         << std::endl;
+    std::cout << "Element at 1st index of type char: "
+         << v1.get(1) << std::endl;
+ 
+    v.pop();
+    v1.pop();
+ 
+    std::cout << "\nAfter deleting last element" << std::endl;
+ 
+    std::cout << "Vector size of type int: " << v.size() << std::endl;
+    std::cout << "Vector size of type char: " << v1.size()
+         << std::endl;
+    std::cout << "Vector capacity of type int : "
+         << v.getcapacity() << std::endl;
+    std::cout << "Vector capacity of type char : "
+         << v1.getcapacity() << std::endl;
+ 
+    std::cout << "Vector elements of type int: ";
+    v.print();
+    std::cout << "Vector elements of type char: ";
+    v1.print();
+ 
+    return 0;
 }

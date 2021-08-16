d9c017008872f0f7ea395ed58e0ea75e3517777c
gvenet <venetguillaume@gmail.com>
2021-08-16 12:06:29 +0200

    1

---
+++ includes/ft_vector.hpp
@@ -0,0 +1,20 @@
+#ifndef FT_VECTOR_HPP
+#define FT_VECTOR_HPP
+
+#include <iostream>
+
+class Ft_vector
+{
+private:
+public:
+	Ft_vector();
+	Ft_vector(Ft_vector const &cpy);
+	Ft_vector &operator=(Ft_vector const &op);
+	virtual ~Ft_vector();
+
+	//========GETTERS========
+	//========SETTERS========
+	//========METHODS========
+};
+
+#endif
---
+++ srcs/ft_vector.cpp
@@ -0,0 +1,17 @@
+#include "../includes/ft_vector.hpp"
+
+Ft_vector::Ft_vector()
+{
+}
+
+Ft_vector::~Ft_vector()
+{
+}
+
+Ft_vector::Ft_vector(Ft_vector const &cpy)
+{
+}
+
+Ft_vector &Ft_vector::operator=(Ft_vector const &op)
+{
+}

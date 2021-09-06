b3de1f558f0668ae2f16ecdad780dbbd3d719793
Guillaume Venet <gvenet@z3r8p3.42lyon.fr>
2021-09-06 17:36:53 +0200

    1

--- inc/Stack.hpp
+++ inc/Stack.hpp
@@ -1,77 +1,71 @@
 #ifndef STACK_HPP
 #define STACK_HPP
-// #include "Vector.hpp"
-#include <vector>
+#include "Vector.hpp"
 
 namespace ft {
 
-// template <class _Tp, class _Container = ft::Vector<_Tp> >
-template <class _Tp, class _Container = std::vector<_Tp> >
-class Stack;
-
-template <class _Tp, class _Container>
-
+template <class T, class Container = ft::Vector<T> >
 class Stack {
-   private:
-    typedef _Container container_type;
-    typedef typename container_type::value_type value_type;
-    typedef typename container_type::reference reference;
-    typedef typename container_type::const_reference const_reference;
-    typedef typename container_type::size_type size_type;
+private:
+	typedef Container								 container_type;
+	typedef typename container_type::value_type		 value_type;
+	typedef typename container_type::reference		 reference;
+	typedef typename container_type::const_reference const_reference;
+	typedef typename container_type::size_type		 size_type;
 
-   protected:
-    container_type c;
+protected:
+	container_type c;
 
-   public:
-    Stack() : c() {}
-    Stack(const Stack& __q) : c(__q.c) {}
-    Stack& operator=(const Stack& __q) {
-        c = __q.c;
-        return *this;
-    }
+public:
+	Stack() : c() {}
+	Stack(const Stack &__q) : c(__q.c) {}
+	Stack &operator=(const Stack &__q) {
+		c = __q.c;
+		return *this;
+	}
 
-    bool empty() const { return c.empty(); }
-    size_type size() const { return c.size(); }
-    reference top() { return c.back(); }
-    const_reference top() const { return c.back(); }
-    void push(const value_type& __v) { c.push_back(__v); }
-    void pop() { c.pop_back(); }
+	bool			empty() const { return c.empty(); }
+	size_type		size() const { return c.size(); }
+	reference		top() { return c.back(); }
+	const_reference top() const { return c.back(); }
+	void			push(const value_type &__v) { c.push_back(__v); }
+	void			pop() { c.pop_back(); }
 };
 
-template <class _Tp, class _Container>
-bool operator==(const Stack<_Tp, _Container>& __x,
-                const Stack<_Tp, _Container>& __y) {
-    return __x.c == __y.c;
+template <class T, class Container>
+bool operator==(const Stack<T, Container> &__x,
+				const Stack<T, Container> &__y) {
+	return __x.c == __y.c;
 }
 
-template <class _Tp, class _Container>
-bool operator<(const Stack<_Tp, _Container>& __x,
-               const Stack<_Tp, _Container>& __y) {
-    return __x.c < __y.c;
+template <class T, class Container>
+bool operator<(const Stack<T, Container> &__x,
+			   const Stack<T, Container> &__y) {
+	return __x.c < __y.c;
 }
 
-template <class _Tp, class _Container>
-bool operator!=(const Stack<_Tp, _Container>& __x,
-                const Stack<_Tp, _Container>& __y) {
-    return !(__x == __y);
+template <class T, class Container>
+bool operator!=(const Stack<T, Container> &__x,
+				const Stack<T, Container> &__y) {
+	return !(__x == __y);
 }
 
-template <class _Tp, class _Container>
-bool operator>(const Stack<_Tp, _Container>& __x,
-               const Stack<_Tp, _Container>& __y) {
-    return __y < __x;
+template <class T, class Container>
+bool operator>(const Stack<T, Container> &__x,
+			   const Stack<T, Container> &__y) {
+	return __y < __x;
 }
 
-template <class _Tp, class _Container>
-bool operator>=(const Stack<_Tp, _Container>& __x,
-                const Stack<_Tp, _Container>& __y) {
-    return !(__x < __y);
+template <class T, class Container>
+bool operator>=(const Stack<T, Container> &__x,
+				const Stack<T, Container> &__y) {
+	return !(__x < __y);
 }
 
-template <class _Tp, class _Container>
-bool operator<=(const Stack<_Tp, _Container>& __x,
-                const Stack<_Tp, _Container>& __y) {
-    return !(__y < __x);
+template <class T, class Container>
+bool operator<=(const Stack<T, Container> &__x,
+				const Stack<T, Container> &__y) {
+	return !(__y < __x);
 }
 
 }  // namespace ft

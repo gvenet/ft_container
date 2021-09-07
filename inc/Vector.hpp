8c56e64378b17cbec7123bd8d290c6bbb71a3e00
Guillaume Venet <gvenet@z3r10p1.42lyon.fr>
2021-09-07 16:37:15 +0200

    1

--- inc/Vector.hpp
+++ inc/Vector.hpp
@@ -1,34 +1,45 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
+#include <iostream>
+#include <iterator>
 #include <memory>
 
 namespace ft {
 
-template <class T, class Allocator = std::allocator<T> > // generic template
+template <class T, class Alloc = std::allocator<T> >
 class Vector {
 public:
+	class Iterator;
+	class Const_Iterator;
 	typedef T										 value_type;
-	typedef Allocator								 allocator_type;
+	typedef Alloc									 allocator_type;
 	typedef typename allocator_type::reference		 reference;
 	typedef typename allocator_type::const_reference const_reference;
-	// typedef implementation-defined                   iterator
-	// typedef implementation-defined                   const_iterator;
-	typedef typename allocator_type::size_type		 size_type;
-	typedef typename allocator_type::difference_type difference_type;
 	typedef typename allocator_type::pointer		 pointer;
 	typedef typename allocator_type::const_pointer	 const_pointer;
+	typedef Iterator								 iterator;
+	typedef Const_Iterator							 const_iterator;
 	typedef std::reverse_iterator<iterator>			 reverse_iterator;
 	typedef std::reverse_iterator<const_iterator>	 const_reverse_iterator;
+	typedef typename allocator_type::size_type		 size_type;
+	typedef typename allocator_type::difference_type difference_type;
 
+private:
+	size_type  _n;
+	value_type _val;
+	Iterator   _first;
+	Iterator   _last;
+
+public:
 	//MEMBERS FUNCTIONS
-	Vector();																								   //constructors
-	Vector(const allocator_type &alloc = allocator_type());													   //_default (1)
-	Vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type()); //_fill (2)
-	template <class InputIterator>																			   //_range (3)
-	Vector(InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type());		   //_
-	Vector(const Vector &x);																				   //_copy (4)
-	~Vector();																								   //destructor
-	Vector &operator=(const Vector &x);																		   //operator=
+	Vector(const allocator_type &alloc = allocator_type());
+	// Vector(const allocator_type &alloc = allocator_type()) : _n(NULL), _val(NULL), _first(NULL), _last(NULL) {}
+	Vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type());
+	template <class InputIterator>
+	Vector(InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type());
+	Vector(const Vector &x);
+	~Vector();
+	Vector &operator=(const Vector &x);
 
 	//ITERATORS
 	iterator			   begin();
@@ -61,39 +72,104 @@ public:
 	const_reference back() const;
 
 	//MODIFIERS
-	template <class InputIterator>							  //range (1)
-	void	 assign(InputIterator first, InputIterator last); //
-	void	 assign(size_type n, const value_type &val);	  //fill (2)
+	template <class InputIterator>							   //range (1)
+	void	 assign(InputIterator first, InputIterator last);  //
+	void	 assign(size_type n, const value_type &val);	   //fill (2)
 	void	 push_back(const value_type &x);
 	void	 pop_back();
-	iterator insert(iterator position, const value_type &val);					 // single element (1)
-	void	 insert(iterator position, size_type n, const value_type &val);		 // fill (2)
-	template <class InputIterator>												 // range (3)
-	void	 insert(iterator position, InputIterator first, InputIterator last); //
+	iterator insert(iterator position, const value_type &val);					  // single element (1)
+	void	 insert(iterator position, size_type n, const value_type &val);		  // fill (2)
+	template <class InputIterator>												  // range (3)
+	void	 insert(iterator position, InputIterator first, InputIterator last);  //
 	iterator erase(iterator position);
 	iterator erase(iterator first, iterator last);
 	void	 swap(Vector &x);
 	void	 clear();
 
 	//ALLOCATOR
-	allocator_type get_allocator() const; //get_allocator
+	allocator_type get_allocator() const;
+
+	//CLASS ITERATOR
+	class Iterator : public std::iterator<std::random_access_iterator_tag, T> {
+	public:
+		Iterator() : p(0) {}
+		Iterator(T *x) : p(x) {}
+		Iterator(const Iterator &mit) : p(mit.p) {}
+
+		Iterator &operator++() {
+			++p;
+			return *this;
+		}
+
+		Iterator operator++(int) {
+			Iterator tmp(*this);
+
+			operator++();
+			return tmp;
+		}
+
+		Iterator &operator--() {
+			--p;
+			return *this;
+		}
+
+		Iterator operator--(int) {
+			Iterator tmp(*this);
+
+			operator--();
+			return tmp;
+		}
+
+		Iterator operator-(int x) {
+			// Iterator tmp(*this);
+
+			for ( int i = 0; i < x; i++ ) {
+				// tmp.operator--();
+				operator--();
+			}
+			return *this;
+		}
+
+		Iterator operator+(int x) {
+			Iterator tmp(*this);
+
+			for ( int i = 0; i < x; i++ )
+				tmp.operator++();
+			return tmp;
+		}
+
+		bool operator<(const Iterator &rhs) const { return p < rhs.p; }
+		bool operator<=(const Iterator &rhs) const { return p <= rhs.p; }
+		bool operator>(const Iterator &rhs) const { return p > rhs.p; }
+		bool operator>=(const Iterator &rhs) const { return p >= rhs.p; }
+		bool operator==(const Iterator &rhs) const { return p == rhs.p; }
+		bool operator!=(const Iterator &rhs) const { return p != rhs.p; }
+
+		T &operator*() { return *p; }
+
+	private:
+		T *p;
+	};
+
+	class ConstIterator {
+		typedef typename std::random_access_iterator_tag iterator_category;
+	};
 };
 
-template <class T, class Allocator>
-void swap(Vector<T, Allocator> &x, Vector<T, Allocator> &y);
+template <class T, class Alloc>
+void swap(Vector<T, Alloc> &x, Vector<T, Alloc> &y);
+template <class T, class Alloc>
+bool operator==(const Vector<T, Alloc> &lhs, const Vector<T, Alloc> &rhs);
+template <class T, class Alloc>
+bool operator!=(const Vector<T, Alloc> &lhs, const Vector<T, Alloc> &rhs);
+template <class T, class Alloc>
+bool operator<(const Vector<T, Alloc> &lhs, const Vector<T, Alloc> &rhs);
+template <class T, class Alloc>
+bool operator<=(const Vector<T, Alloc> &lhs, const Vector<T, Alloc> &rhs);
+template <class T, class Alloc>
+bool operator>(const Vector<T, Alloc> &lhs, const Vector<T, Alloc> &rhs);
+template <class T, class Alloc>
+bool operator>=(const Vector<T, Alloc> &lhs, const Vector<T, Alloc> &rhs);
 
-template <class T, class Allocator>
-bool operator==(const Vector<T, Allocator> &lhs, const Vector<T, Allocator> &rhs);
-template <class T, class Allocator>
-bool operator!=(const Vector<T, Allocator> &lhs, const Vector<T, Allocator> &rhs);
-template <class T, class Allocator>
-bool operator<(const Vector<T, Allocator> &lhs, const Vector<T, Allocator> &rhs);
-template <class T, class Allocator>
-bool operator<=(const Vector<T, Allocator> &lhs, const Vector<T, Allocator> &rhs);
-template <class T, class Allocator>
-bool operator>(const Vector<T, Allocator> &lhs, const Vector<T, Allocator> &rhs);
-template <class T, class Allocator>
-bool operator>=(const Vector<T, Allocator> &lhs, const Vector<T, Allocator> &rhs);
-
-} // namespace ft
+}  // namespace ft
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,25 +1,67 @@
-#include <algorithm>
-#include <iostream>
-#include <stack>
-#include <vector>
-#include "../inc/Stack.hpp"
 #include "../inc/Vector.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
 
-int main()
-{
-	ft::Stack<int> s1;
-	// std::stack<int> s1;
+typedef ft::Vector<int>::Iterator Iterator;
 
-	s1.push(10);
-	s1.push(20);
-	s1.push(30);
+int main() {
+	std::vector<int>		   v1(10, 10);
+	std::vector<int>::iterator it;
 
-	std::cout << s1.top() << std::endl;
-	s1.pop();
-	std::cout << s1.top() << std::endl;
-	s1.pop();
-	std::cout << s1.top() << std::endl;
-	s1.pop();
+	it = v1.begin();
+	std::cout << *it << std::endl;
 
 	return 0;
 }
+
+// int main() {
+
+// 	int numbers[] = {10, 20, 30, 40, 50, 60};
+
+// 	Iterator begin(numbers);
+// 	Iterator end(numbers + 6);
+// 	Iterator it;
+
+// 	for ( it = begin; it != end; it++ )
+// 		std::cout << *it << " ";
+// 	std::cout << "\n------------\n";
+// 	it = end - 1;
+// 	for ( ; it >= begin ; it-- )
+// 		std::cout << *it << " ";
+// 	std::cout << "\n------------\n";
+// 	for ( it = begin; it < end; it++ )
+// 		std::cout << *it << " ";
+// 	std::cout << "\n------------\n";
+// 	it = begin + 5;
+// 	std::cout << *it << std::endl;
+// 	it = end - 6;
+// 	std::cout << *it << std::endl;
+
+// 	return 0;
+// }
+
+// int main() {
+// 	std::vector<int> numbers;
+// 	std::vector<int>::iterator it;
+
+// 	numbers.push_back(0);
+// 	numbers.push_back(10);
+// 	numbers.push_back(20);
+// 	numbers.push_back(30);
+// 	numbers.push_back(40);
+// 	numbers.push_back(50);
+
+// 	for (it = numbers.begin(); it < numbers.end(); it++ )
+// 		std::cout << *it << " ";
+// 	std::cout << std::endl;
+// 	// for (it = numbers.end() - 1; it != numbers.begin() - 1; --it )
+// 		// std::cout << *it << " ";
+// 	// std::cout << std::endl;
+
+// 	// std::cout << numbers.begin() << std::endl;
+
+// 	std::cout << (it < numbers.end()) << std::endl;
+
+// 	return 0;
+// }

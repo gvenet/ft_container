39be28660b4511bb210f960d212c3ec34b2df7ed
Guillaume Venet <gvenet@z3r11p3.42lyon.fr>
2021-09-08 10:44:35 +0200

    1

--- inc/Utils.hpp
+++ inc/Utils.hpp
@@ -14,4 +14,21 @@ private:
 
 } u_nullptr = {};
 
+namespace ft {
+
+template <class Category, class T, class Distance = ptrdiff_t,
+		  class Pointer = T*, class Reference = T&>
+class iterator {
+public:
+	typedef T		  value_type;
+	typedef Distance  difference_type;
+	typedef Pointer	  pointer;
+	typedef Reference reference;
+	typedef Category  iterator_category;
+};
+
+class random_access_iterator_tag { };
+
+}  // namespace ft
+
 #endif

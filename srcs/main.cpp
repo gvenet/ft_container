a881702212d232236e0a8bfd1fc22180f0728325
gvenet <venetguillaume@gmail.com>
2021-09-09 11:57:29 +0200

    1

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -22,11 +22,6 @@ void test_std(int i) {
 		std::cout << v1.size() << " " << v1.capacity() << " | ";
 	}
 }
-// void test2(int i) {
-// 	std::vector<int> v1(i, 5);
-// 	v1.insert(v1.end(), 42);
-// 	std::cout << v1.size() << " " << v1.capacity() << "\n";
-// }
 
 int main() {
 	int r = 10;
@@ -40,39 +35,6 @@ int main() {
 		std::cout << std::endl;
 	}
 
-	// ft::Vector<int>::iterator it;
-
-	// for ( it = v1.begin(); it != v1.end(); it++ )
-	// 	std::cout << *it << " ";
-	// std::cout << std::endl;
-
 	return 0;
 }
 
-// void resize_display(int n, size_t val, std::vector<int> &v1) {
-// 	v1.resize(n, val);
-// 	for_each(v1.begin(), v1.end(), print);
-// 	std::cout << std::endl;
-// }
-
-// int main(void) {
-// 	std::vector<int>		   v1(10, 5);
-// 	std::vector<int>::iterator it;
-
-// 	resize_display(20, 2, v1);
-// 	resize_display(2, 1, v1);
-// 	resize_display(10, 9, v1);
-// 	resize_display(20, 0, v1);
-// 	resize_display(0, 0, v1);
-// 	resize_display(10, 9, v1);
-// 	resize_display(20, 3, v1);
-// 	resize_display(15, 4, v1);
-// 	v1.insert(v1.end(), 42);
-// 	for_each(v1.begin(), v1.end(), print);
-// 	std::cout << std::endl;
-// 	v1.insert(v1.begin(), 42);
-// 	for_each(v1.begin(), v1.end(), print);
-// 	std::cout << std::endl;
-
-// 	return 0;
-// }
--- tester/srcs/main.cpp
+++ tester/srcs/main.cpp
@@ -6,24 +6,45 @@
 /*   By: gvenet <gvenet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 17:53:07 by rchallie          #+#    #+#             */
-/*   Updated: 2021/09/08 16:20:28 by gvenet           ###   ########.fr       */
+/*   Updated: 2021/09/09 11:36:30 by gvenet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "../inc/vector.hpp"
 
-int main() {
-	ft::vector<int>			  v1(3, 5);
-	ft::vector<int>			  v2(3);
-	ft::vector<int>			  v3;
+static int rf = 20;
 
-	ft::vector<int>::iterator it;
-
-	v1.insert(v1.begin(),42);
-	for ( it = v1.begin(); it != v1.end(); it++ ) {
-		std::cout << *it << " ";
+void test_ft(int i) {
+	ft::vector<std::string> v1(i, "toto");
+	for ( int i = 0; i < rf; i++ ) {
+		v1.insert(v1.end(), "titi");
+		std::cout << v1.size() << " " << v1.capacity() << " | ";
 	}
-	std::cout << std::endl;
+}
+
+void test_std(int i) {
+	std::vector<std::string> v1(i, "toto");
+	for ( int i = 0; i < rf; i++ ) {
+		v1.insert(v1.end(), "titi");
+		std::cout << v1.size() << " " << v1.capacity() << " | ";
+	}
+}
+
+int main() {
+	int r = 10;
+
+	std::cout << "S C\n\n";
+	for ( int i = 0; i < r; i++ ) {
+		test_ft(i);
+		std::cout << std::endl;
+		test_std(i);
+		std::cout << std::endl;
+		std::cout << std::endl;
+	}
+
+	return 0;
 }

29b501157abcf8b680a3f83c0293e1ca62e617b3
Guillaume Venet <gvenet@z3r9p5.42lyon.fr>
2021-10-13 10:04:19 +0200

    1

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,47 +1,31 @@
 #include <algorithm>
+#include <string>
+#include <iostream>
+
 
 #include "../inc/containers/map.hpp"
 #include "../inc/containers/vector.hpp"
 
-void fct(ft::pair<std::string, int> &p) {
-	std::cout << p.first << " " << p.second << "\n";
-}
 
-void printMap(ft::map<std::string, int> &m) {
-	for_each(m.begin(), m.end(), fct);
-}
-
-void test29() {
-	typedef ft::map<std::string, int> msi;
-	std::string						  st[] = {"A06", "A03", "A09", "A01", "A08"};
-	size_t							  n = sizeof(st) / sizeof(st[0]);
-	msi								  m;
-
-	while ( n-- )
-		m.insert(ft::make_pair(st[n], n));
-
-	printMap(m);
-
-	std::cout << "\n";
-	msi m2(m);
-	printMap(m2);
-}
-
-void test30() {
-	typedef ft::vector<int> vi;
-	typedef vi::iterator	vi_it;
-	vi						v;
-
-	v.push_back(42);
-	v.push_back(18);
-	v.push_back(3);
-
-	for ( vi_it vit = v.begin(); vit != v.end(); vit++ ) {
-		std::cout << *vit << "\n";
-	}
+template <class C>
+void print_map(C &x) {
+	std::cout << x.second << "\t" << x.first << "\n";
 }
 
 int main() {
-	test29();
-	test30();
+
+	typedef ft::map<std::string, int> msi;
+	typedef ft::pair<std::string, int> psi;
+
+	std::string tab[] = {"jkd", "pwls", "pojdjd", "ok", "terere", "kbmf", "kdmcn"};
+	size_t n = sizeof(tab) / sizeof(tab[0]);
+
+	msi m1;
+
+	for (size_t i = 0; i < n; i++) {
+		m1.insert(ft::make_pair(tab[i], i));
+	}
+
+	for_each(m1.begin(), m1.end(), print_map<psi>);
+	return 0;
 }

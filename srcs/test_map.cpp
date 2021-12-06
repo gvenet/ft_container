dfd36b5ac4aee3ab6fd0ea3ad7abfb1d04dc160b
IMac <imac@imac-de-imac.home>
2021-12-06 08:08:54 +0100

    1

--- srcs/test_map.cpp
+++ srcs/test_map.cpp
@@ -1,34 +1,39 @@
 #include "../inc/map.hpp"
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <map>
 
 template <typename T_L, typename T_R>
-std::ostream& operator<<(std::ostream& os, const std::map<T_L,T_R>& m)
-{
+std::ostream& operator<<( std::ostream& os, const std::map<T_L, T_R>& m ) {
 	for ( typename std::map<T_L, T_R>::const_iterator it = m.begin(); it != m.end(); it++ )
-		os << "(" << it->first << "_" << it->second << ")\n";
+		os << "(" << it->first << "_" << it->second << ")";
 	return os;
 }
 
 template <typename T_L, typename T_R>
-std::ostream& operator<<(std::ostream& os, const ft::map<T_L,T_R>& m)
-{
+std::ostream& operator<<( std::ostream& os, const ft::map<T_L, T_R>& m ) {
 	for ( typename ft::map<T_L, T_R>::const_iterator it = m.begin(); it != m.end(); it++ )
-		os << "(" << it->first << "_" << it->second << ")\n";
+		os << "(" << it->first << "_" << it->second << ")";
 	return os;
 }
 
-template <class M, class P>
-void test(P ( *make_pair )( int, int )) {
+template <class M, class P, typename T_L, typename T_R>
+void test( P ( *make_pair )( T_L, T_R ) ) {
 	M m;
 
-	m.insert( make_pair( 42, 18 ) );
-
+	m.insert( make_pair( 65, 18 ) );
 	std::cout << m << "\n";
 }
 
+template <typename T_L, typename T_R>
+void type_choice() {
+	std::cout << "__FT__\n";
+	test<ft::map<T_L, T_R>, ft::pair<T_L, T_R> >( &( ft::make_pair<T_L, T_R> ) );
+	std::cout << "__STD__\n";
+	test<std::map<T_L, T_R>, std::pair<T_L, T_R> >( &( std::make_pair<T_L,T_R> ) );
+}
+
 int main() {
-	test<ft::map<int, int>, ft::pair<int, int> >(&(ft::make_pair));
-	test<std::map<int, int>, std::pair<int, int> >(&(std::make_pair));
+	type_choice<int,int>();
+	type_choice<char,int>();
 }

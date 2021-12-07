a190556af83ef133f04f26bbb9e6865b4867a787
Guillaume Venet <gvenet@z3r6p1.42lyon.fr>
2021-12-07 10:40:22 +0100

    1

--- srcs/map_insert_access.cpp
+++ srcs/map_insert_access.cpp
@@ -6,15 +6,23 @@
 #include <string>
 #include <sys/time.h>
 
-typedef void(func_type)();
+#define GRN "\033[01;31m"
+#define RED "\033[01;32m"
+#define YLW "\033[01;33m"
+#define CBN "\033[0m"
+
+typedef void( func_type )();
+
+#define A1 10000
+#define A2 100000
 
 void test_insert_access_ft() {
 	ft::map<int, int> map_int;
-	for ( int i = 0; i < 1000000; ++i ) {
+	for ( int i = 0; i < A1; ++i ) {
 		map_int.insert( ft::make_pair( rand(), rand() ) );
 	}
 	int sum = 0;
-	for ( int i = 0; i < 100000; i++ ) {
+	for ( int i = 0; i < A2; i++ ) {
 		int access = rand() % map_int.size();
 		sum += map_int[access];
 	}
@@ -22,17 +30,17 @@ void test_insert_access_ft() {
 
 void test_insert_access_std() {
 	std::map<int, int> map_int;
-	for ( int i = 0; i < 1000000; ++i ) {
+	for ( int i = 0; i < A1; ++i ) {
 		map_int.insert( std::make_pair( rand(), rand() ) );
 	}
 	int sum = 0;
-	for ( int i = 0; i < 100000; i++ ) {
+	for ( int i = 0; i < A2; i++ ) {
 		int access = rand() % map_int.size();
 		sum += map_int[access];
 	}
 }
 
-double exec_time( func_type* test_func) {
+double exec_time( func_type* test_func ) {
 	std::clock_t start, end;
 	start = clock();
 	test_func();
@@ -43,14 +51,14 @@ double exec_time( func_type* test_func) {
 
 void ratio( double ft, double std ) {
 	double ratio = ft / std;
-	std::cout << "\033[01;33mft  = \033[0m" << ft << "\n";
-	std::cout << "\033[01;33mstd = \033[0m" << std << "\n";
-	std::cout << "\033[01;33mratio ft / std = \033[0m";
+	std::cout << YLW << "ft  = " << CBN << ft << std::endl;
+	std::cout << YLW << "std = " << CBN << std << std::endl;
+	std::cout << YLW << "ratio ft / std = " << CBN;
 	if ( ratio > 20 )
-		std::cout << "\033[01;31m";
+		std::cout << GRN;
 	else
-		std::cout << "\033[01;32m";
-	std::cout << ratio << " \033[0m\n";
+		std::cout << RED;
+	std::cout << ratio << CBN << std::endl;
 }
 
 int main() {
